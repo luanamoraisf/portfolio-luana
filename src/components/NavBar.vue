f1a6f45feb3398df7ae0afa26ecf87fd51bbe6de
luanamoraisf <luanaflorenciopc@gmail.com>
2025-02-28 18:40:36 -0300

    changes positioning and display of the navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,8 +1,8 @@
 <template>
   <Disclosure as="nav" v-slot="{ open }">
-    <div class="mx-auto max-w-7xl px-2 lg:px-6 lg:px-8" style="position: fixed; background-color: #1A1D22; border: 10px solid #1A1D22; margin-top: -2.2%; z-index: 1; cursor: pointer;">
-      <div class="relative flex h-16 justify-between ml-80">
-        <div class="absolute inset-y-0 left-0 flex items-center lg:hidden">
+    <div class="mx-auto max-w-7xl px-2 lg:px-6 lg:px-8" style="position: fixed; z-index: 1; cursor: pointer;">
+      <div class="relative flex h-16 justify-between ml-20">
+        <div class="absolute inset-y-0 left-0 flex items-center nx:hidden">
           <!-- Mobile menu button-->
           <DisclosureButton class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
             <span class="absolute -inset-0.5" />
@@ -12,9 +12,9 @@
           </DisclosureButton>
         </div>
         <div class="flex flex-1 lg:items-stretch lg:justify-start">
-          <div class="hidden lg:ml-6 lg:block">
-            <div class="flex space-x-4" style="margin-top: 1.6%;">
-              <a v-for="item in navigation" :key="item.name" v-scroll-to="item.href" :class="[item.current ? 'bg-[#be61c0] text-white hover:bg-[#6d256e]' : 'text-gray-300 hover:bg-[#6d256e]', 'rounded-sm px-3 py-2 text-lg font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+          <div class="hidden nx:ml-6 nx:block">
+            <div class="flex space-x-4" style="text-shadow: 2px 2px 4px rgba(0, 0, 0);">
+              <a v-for="item in navigation" :key="item.name" v-scroll-to="item.href" :class="[item.current ? 'bg-[#be61c0] text-white hover:bg-[#6d256e] font-bold' : 'text-gray-300 hover:bg-[#6d256e]', 'rounded-sm px-3 py-2 text-lg font-bold drop-shadow-xl']" :aria-current="item.current ? 'page' : undefined">{{ item.name }} </a>
             </div>
           </div>
         </div>
